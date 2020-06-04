52e7cc79a5647d297e103c103bf3760e4d25c1e8
Vincent Sanders <vincesanders@hotmail.com>
2020-06-04 17:25:17 -0500

    Finished 5th challenge. STRETCHgit add .

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,13 +1,32 @@
 # Your code here
-
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
-
+    result = []
+    # iterate over each path
+    # file is key, rest of path is value
+    paths = {}
+    for path in files:
+        # iterate backward through path
+        index = len(path) - 1
+        char = path[index]
+        key = ''
+        # add file names to paths
+        while char != '/':
+            key = char + key
+            index -= 1
+            char = path[index]
+        index = index + 1
+        if key in paths:
+            paths[key].append(path[:index])
+        else:
+            paths[key] = [path[:index]]
+    for q in queries:
+        if q in paths:
+            for i in range(len(paths[q])):
+                result.append(f'{paths[q][i]}{q}')
     return result
 
 
