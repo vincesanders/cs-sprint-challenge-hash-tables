385e0197b35970197532943c206f580c0c53abf4
Vincent Sanders <vincesanders@hotmail.com>
2020-06-04 13:57:47 -0500

    Completed challenge 1.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,18 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    values = {}
+    for i in range(len(weights)):
+        if weights[i] not in values:
+            values[weights[i]] = i
+        co_key = limit - weights[i]
+        if co_key in values and values[co_key] is not i:
+            co_index = values[co_key]
+            if i >= co_index:
+                return [i, co_index]
+            else:
+                return [values[co_key], i]
     return None
+
+weights_4 = [4,4]
+print(get_indices_of_item_weights(weights_4, 4, 8))
