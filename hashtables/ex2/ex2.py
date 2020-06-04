b8dfcc0be4d66f337ca726a387b7bb6b459d5ae8
Vincent Sanders <vincesanders@hotmail.com>
2020-06-04 16:19:05 -0500

    Solved challenge 2 and 3.

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,21 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
-
+    flights = {}
+    route = []
+    for ticket in tickets:
+        flights[ticket.source] = ticket.destination
+    current_flight = flights['NONE']
+    while current_flight != 'NONE':
+        route.append(current_flight)
+        current_flight = flights[current_flight]
+    route.append(current_flight)
     return route
+
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
+
+# tickets = [ticket_1, ticket_2, ticket_3]
+
+# print(reconstruct_trip(tickets, len(tickets)))
--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,10 +1,32 @@
-def intersection(arrays):
+def intersection(arrays): # O(2kn) => O(n)
     """
     YOUR CODE HERE
     """
     # Your code here
-
-    return result
+    numbers = {'intersection': []}
+    #find longest array in arrays
+    k = len(arrays)
+    n = 0
+    for i in range(k):
+        if len(arrays[i]) > n:
+            n = len(arrays[i])
+    # iterate through that loop
+    for i in range(n):
+        # at each iteration, add the number at that index from all other arrays to numbers with a value of 1
+        for j in range(k):
+            if i > len(arrays[j]) - 1:
+                continue
+            num = arrays[j][i]
+            if num in numbers:
+                # increase the value of a number in numbers if the number is found again
+                numbers[num] += 1
+                # if the amount of a number reaches the number of arrays it is added to numbers.intersection
+                if numbers[num] == k:
+                    numbers['intersection'].append(num)
+            else:
+                numbers[num] = 1
+    #return numbers.'intersection'
+    return numbers['intersection']
 
 
 if __name__ == "__main__":
@@ -13,5 +35,13 @@ if __name__ == "__main__":
     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
+    # arrays = [
+    #     [1, 2, 3, 4, 5],
+    #     [12, 7, 2, 9, 1],
+    #     [99, 2, 7, 1,]
+    # ]
+
+    # print(arrays)
 
     print(intersection(arrays))
